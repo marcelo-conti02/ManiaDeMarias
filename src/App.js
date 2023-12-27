78d15ddf260baf4b8453d77e1d6b3d31af84099a
Marcelo Conti <marceloconti9017@gmail.com>
2023-12-27 18:43:41 -0300

    adicionei header com input

--- src/App.js
+++ src/App.js
@@ -2,23 +2,50 @@ import { useState } from 'react';
 
 
 export default function ManiaDeMarias() {
+    const [nome, setNome] = useState('');
+    const [aux, setAux] = useState('');
+
+    function handleClick() {
+        setAux(nome);
+    }
+
     return (
         <>
-            <Multiplica />
-            <Multiplica />
+            <div className='header'>
+                <Input nome={nome} setNome={setNome} onClick={handleClick} />
+                <h1 className='mensagem'>Olá, {aux}</h1>
+            </div>
         </>
     );
 }
 
 
-function Multiplica(){
+function Input({ nome, setNome, onClick }) {
+
+    return (
+        <>
+            <div className='input'>
+                <label>Nome: </label>
+                <input
+                    type='text'
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                    placeholder='Insira seu nome'
+                    required />
+                <button onClick={onClick}>Submit</button>
+            </div>
+        </>
+    );
+}
+
+function Multiplica() {
     const [valor, setValor] = useState(2);
 
-    function clicou(){
+    function clicou() {
         setValor(valor * 2);
     }
 
-    return(
+    return (
         <>
             <h1>{valor}</h1>
             <button onClick={clicou}>multiplica por 2</button>
@@ -26,3 +53,5 @@ function Multiplica(){
     );
 
 }
+
+
