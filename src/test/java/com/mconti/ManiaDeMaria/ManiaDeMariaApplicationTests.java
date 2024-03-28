e11d94dcbcbd3645d4bea816e4e2b984c09af277
marcelo-conti02 <marceloconti9017@gmail.com>
2024-03-28 20:57:27 -0300

    Implementa a estrutura do projeto utilizando Spring Boot em Java.

---
+++ src/test/java/com/mconti/ManiaDeMaria/ManiaDeMariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mconti.ManiaDeMaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManiaDeMariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
