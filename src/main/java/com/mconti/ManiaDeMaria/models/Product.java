e4442dbdbf0ea6299c5272d64155a281240da297
marcelo-conti02 <marceloconti9017@gmail.com>
2024-04-09 11:35:05 -0300

    Adiciona métodos para a classe Product

--- src/main/java/com/mconti/ManiaDeMaria/models/Product.java
+++ src/main/java/com/mconti/ManiaDeMaria/models/Product.java
@@ -5,23 +5,73 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
-@Table(name = Product.TABLE_NAME)
 public class Product {
-        public static final String TABLE_NAME = "product";
-
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "id", unique = true)
         private Long id;
 
         @Column(name = "product_name", length = 100, nullable = false, unique = true)
+        @NotNull
+        @NotEmpty
         private String productName;
 
         @Column(name = "product_type", length = 100, nullable = false)
+        @NotNull
+        @NotEmpty
         private String productType;
 
+        public Product() {
+        }
 
+        public Product(Long id, String productName, String productType) {
+                this.id = id;
+                this.productName = productName;
+                this.productType = productType;
+        }
+
+        public Long getId() {
+                return this.id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getProductName() {
+                return this.productName;
+        }
+
+        public void setProductName(String productName) {
+                this.productName = productName;
+        }
+
+        public String getProductType() {
+                return this.productType;
+        }
+
+        public void setProductType(String productType) {
+                this.productType = productType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (o == this)
+                        return true;
+                if (!(o instanceof Product)) {
+                        return false;
+                }
+                Product product = (Product) o;
+                return Objects.equals(id, product.id) && Objects.equals(productName, product.productName)
+                                && Objects.equals(productType, product.productType);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(id, productName, productType);
+        }
 }
