2b23e6ad442a4dc1a6c44485f68587b190d9c45e
marcelo <marceloconti9017@gmail.com>
2024-05-16 15:42:47 -0300

    Modifica o WebConfig
    Adiciona a origem e os métodos permitidos

--- src/main/java/com/mconti/ManiaDeMaria/configs/WebConfig.java
+++ src/main/java/com/mconti/ManiaDeMaria/configs/WebConfig.java
@@ -9,7 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 
+    @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
