15137d2e804088536f56c6f3163d95f29353a7c5
marcelo-conti02 <marceloconti9017@gmail.com>
2024-05-24 17:11:59 -0300

    Adiciona a dependencia lombok

--- src/main/java/com/mconti/ManiaDeMaria/models/Product.java
+++ src/main/java/com/mconti/ManiaDeMaria/models/Product.java
@@ -7,9 +7,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import java.util.Objects;
+import lombok.Data;
 
 @Entity
+@Data
 public class Product {
         public interface CreateProduct{}
         public interface UpdateProduct{}
@@ -31,65 +32,4 @@ public class Product {
         @Column(name = "product_price", length = 50, nullable = false)
         @NotNull(groups = {CreateProduct.class, UpdateProduct.class})
         private Float productPrice;
-
-        public Product() {
-        }
-
-        public Product(Long id, String productName, String productType, Float productPrice) {
-                this.id = id;
-                this.productName = productName;
-                this.productType = productType;
-                this.productPrice = productPrice;
-        }
-
-        public Long getId() {
-                return this.id;
-        }
-
-        public void setId(Long id) {
-                this.id = id;
-        }
-
-        public String getProductName() {
-                return this.productName;
-        }
-
-        public void setProductName(String productName) {
-                this.productName = productName;
-        }
-
-        public String getProductType() {
-                return this.productType;
-        }
-
-        public void setProductType(String productType) {
-                this.productType = productType;
-        }
-
-        public Float getProductPrice() {
-                return this.productPrice;
-        }
-
-        public void setProductPrice(Float productPrice) {
-                this.productPrice = productPrice;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-                if (o == this)
-                        return true;
-                if (!(o instanceof Product)) {
-                        return false;
-                }
-                Product product = (Product) o;
-                return Objects.equals(id, product.id) 
-                                && Objects.equals(productName, product.productName)
-                                && Objects.equals(productType, product.productType) 
-                                && Objects.equals(productPrice, product.productPrice);
-        }
-
-        @Override
-        public int hashCode() {
-                return Objects.hash(id, productName, productType, productPrice);
-        }
 }
