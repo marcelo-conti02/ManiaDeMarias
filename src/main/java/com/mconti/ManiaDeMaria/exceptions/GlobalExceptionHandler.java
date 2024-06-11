846d534c61677d37dbcfa24522fcf1e73383bee6
marcelo <marceloconti9017@gmail.com>
2024-06-11 17:08:42 -0300

    Set UserService authorization

--- src/main/java/com/mconti/ManiaDeMaria/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mconti/ManiaDeMaria/exceptions/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.mconti.ManiaDeMaria.services.exceptions.AuthorizationException;
 import com.mconti.ManiaDeMaria.services.exceptions.ObjectNotFoundException;
 
 import jakarta.servlet.ServletException;
@@ -97,6 +99,42 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler imple
         return buildErrorResponse(
                 objectNotFoundException,
                 HttpStatus.NOT_FOUND,
+                request);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<Object> handleAuthenticationException(
+            AuthenticationException authenticationException,
+            WebRequest request) {
+        log.error("Authentication error ", authenticationException);
+        return buildErrorResponse(
+                authenticationException,
+                HttpStatus.UNAUTHORIZED,
+                request);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object> handleAccessDeniedException(
+            AccessDeniedException accessDeniedException,
+            WebRequest request) {
+        log.error("Authorization error ", accessDeniedException);
+        return buildErrorResponse(
+                accessDeniedException,
+                HttpStatus.FORBIDDEN,
+                request);
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object> handleAuthorizationException(
+            AuthorizationException authorizationException,
+            WebRequest request) {
+        log.error("Authorization error ", authorizationException);
+        return buildErrorResponse(
+                authorizationException,
+                HttpStatus.FORBIDDEN,
                 request);
     }
 
--- src/main/java/com/mconti/ManiaDeMaria/services/UserService.java
+++ src/main/java/com/mconti/ManiaDeMaria/services/UserService.java
@@ -1,11 +1,13 @@
 package com.mconti.ManiaDeMaria.services;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mconti.ManiaDeMaria.models.User;
 import com.mconti.ManiaDeMaria.models.enums.ProfileEnum;
 import com.mconti.ManiaDeMaria.repositories.UserRepository;
+import com.mconti.ManiaDeMaria.security.UserSpringSecurity;
+import com.mconti.ManiaDeMaria.services.exceptions.AuthorizationException;
 import com.mconti.ManiaDeMaria.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,19 +27,23 @@ public class UserService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return this.userRepository.findAll();
     }
-    
-    public User findById(Long id){
+
+    public User findById(Long id) {
+        UserSpringSecurity userSpringSecurity = authenticated();
+
+        if (!Objects.nonNull(userSpringSecurity)
+                || !userSpringSecurity.hasRole(ProfileEnum.ADMIN) && !id.equals(userSpringSecurity.getId()))
+            throw new AuthorizationException("Acesso negado!");
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException(
-            "Usuário não encontrado! Id:" + id
-        ));
+                "Usuário não encontrado! Id:" + id));
     }
 
     @Transactional
-    public User create(User obj){
+    public User create(User obj) {
         obj.setId(null);
         obj.setUserPassword(this.bCryptPasswordEncoder.encode(obj.getUserPassword()));
         obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
@@ -44,15 +52,23 @@ public class UserService {
     }
 
     @Transactional
-    public User update(User obj){
+    public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setUserPassword(obj.getUserPassword());
         newObj.setUserPassword(this.bCryptPasswordEncoder.encode(obj.getUserPassword()));
         return this.userRepository.save(newObj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         findById(id);
         this.userRepository.deleteById(id);
+    }
+
+    public static UserSpringSecurity authenticated() {
+        try {
+            return (UserSpringSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
