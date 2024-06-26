3a28430156386aac544eba300476ee9cd8e03bf1
marcelo <marceloconti9017@gmail.com>
2024-06-26 14:34:02 -0300

    Update CartProductsService

--- src/main/java/com/mconti/ManiaDeMaria/services/CartProductsService.java
+++ src/main/java/com/mconti/ManiaDeMaria/services/CartProductsService.java
@@ -3,9 +3,14 @@ package com.mconti.ManiaDeMaria.services;
 import com.mconti.ManiaDeMaria.models.Cart;
 import com.mconti.ManiaDeMaria.models.CartProducts;
 import com.mconti.ManiaDeMaria.models.Product;
+import com.mconti.ManiaDeMaria.repositories.CartProductsRepository;
 import com.mconti.ManiaDeMaria.security.Authenticated;
 import com.mconti.ManiaDeMaria.services.CartService;
 import com.mconti.ManiaDeMaria.services.exceptions.AuthorizationException;
+import com.mconti.ManiaDeMaria.services.exceptions.ObjectNotFoundException;
+
+import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,19 +19,43 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CartProductsService {
     @Autowired
+    private CartProductsRepository cartProductsRepository;
+
+    @Autowired
     private CartService cartService;
 
     @Autowired
     private Authenticated authenticated;
 
+    public CartProducts findById(Long id) {
+        authenticated.isAuthenticated();
+        Optional<CartProducts> cartProduct = this.cartProductsRepository.findById(id);
+        return cartProduct.orElseThrow(() -> new ObjectNotFoundException(
+                "CartProduct not found! Id:" + id));
+    }
+
+    public List<CartProducts> findAllById(Iterable<Long> id) {
+        authenticated.isAuthenticated();
+        Optional<List<CartProducts>> cartProducts = Optional.of(this.cartProductsRepository.findAllById(id));
+        return cartProducts.orElseThrow(() -> new ObjectNotFoundException(
+                "CartProduct not found! Id:" + id));
+    }
+
     @Transactional
     public CartProducts create(CartProducts cartProduct, Cart cart, Product product) {
         Long userId = authenticated.isAuthenticated();
-        if(userId != cart.getUserId())
+        if (userId != cart.getUserId())
             throw new AuthorizationException("Acesso negado!");
-        
+
         cartProduct.setCartId(cart.getId());
         cartProduct.setProductId(product.getId());
+        cartService.update(cart, product.getProductPrice());
+        cartProduct = this.cartProductsRepository.save(cartProduct);
+
         return cartProduct;
+    }
+
+    public void delete(CartProducts cartProduct) {
+
     }
 }
