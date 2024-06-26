dc37a9529822381d2f15b76135a0ad04b5c8bb94
marcelo <marceloconti9017@gmail.com>
2024-06-26 08:51:09 -0300

    Update CarService

---
+++ src/main/java/com/mconti/ManiaDeMaria/services/CartProductsService.java
@@ -0,0 +1,12 @@
+package com.mconti.ManiaDeMaria.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CartProductsService {
+    @Transactional
+    public void addProduct(Long id){
+        
+    }
+}
--- src/main/java/com/mconti/ManiaDeMaria/services/CartService.java
+++ src/main/java/com/mconti/ManiaDeMaria/services/CartService.java
@@ -1,5 +1,6 @@
 package com.mconti.ManiaDeMaria.services;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mconti.ManiaDeMaria.models.Cart;
+import com.mconti.ManiaDeMaria.models.enums.ProfileEnum;
 import com.mconti.ManiaDeMaria.repositories.CartRepository;
 import com.mconti.ManiaDeMaria.services.exceptions.AuthorizationException;
 import com.mconti.ManiaDeMaria.services.exceptions.ObjectNotFoundException;
@@ -18,30 +20,30 @@ public class CartService {
     CartRepository cartRepository;
 
     @Transactional
-    public Cart create(Cart cart, Long userId) {
-        isAuthenticated(userId);
+    public Cart create(Cart cart) {
+        Long userId = isAuthenticated();
         cart.setUserId(userId);
         cart.setTotalPrice((float) 0);
         cart = this.cartRepository.save(cart);
         return cart;
     }
 
-    public Cart findById(Long id, Long userId) {
-        isAuthenticated(userId);
+    public Cart findById(Long id) {
+        isAuthenticated();
         Optional<Cart> cart = this.cartRepository.findById(id);
         return cart.orElseThrow(() -> new ObjectNotFoundException(
                 "Cart not found! Id:" + id));
     }
 
     public void delete(Long id, Long userId) {
-        findById(id, userId);
+        findById(id);
         this.cartRepository.deleteById(id);
     }
 
-    private void isAuthenticated(Long userId){
+    private Long isAuthenticated(){
         UserSpringSecurity userSpringSecurity = UserService.authenticated();
-        if (!userId.equals(userSpringSecurity.getId()))
+        if (Objects.isNull(userSpringSecurity))
             throw new AuthorizationException("Acesso negado!");
-        return;
+        return userSpringSecurity.getId();
     }
 }
